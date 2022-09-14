c51db568325755c361df0340d735fccf5af8570b
SanaliP <112032100+sanali48@users.noreply.github.com>
2022-09-14 22:06:11 +0000

    typing the instructions

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ if playing != "yes":
   quit ()
 
 
-print ("okay! Let's play! :)")
-  
+print ("okay! Let's play!")
+print("\n")
+
+def print_instructions():
+  print ("thank you for choosing to play this game")
+  print("in this game you will be tested on your general knowlege")
+  print("you could choose the answer by either using abc or typing the answer")
+  print("\n")
+
+
 score = 0 
+print_instructions()
 
 # QUESTION 1 
 answer1 = input("what is the capital of Queensland? \na. Townsville \nb. Brisbane \nc. Noosa \nAnswer: ") .lower() .strip()
@@ -23,9 +32,9 @@ else:
   print("\n")
 
 
-# QUESTION 2 
+#QUESTION 2 
 answer2 = input("In which state is Briyedsville found? \na. New South Wales \nb. South Australia  \nc. Queensland \nAnswer: ") .lower() .strip()  
-if answer2 == "c" or answer2 == "Queensland": 
+if answer2 == "c" or answer2 == "queensland": 
   score  += 1 
   print("correct!")
   print("score: ", score)
@@ -36,17 +45,37 @@ else:
   print("\n")
 
 
-# QUESTION 2 
-answer1 = input("what is the capital city of Australia? \na. Canberra \nb. Sydney \nc. Noosa \nAnswer: ") .lower() .strip()
-if answer1 == "b" or answer1 == "brisbane": 
+# QUESTION 3 
+answer3 = input("what is the capital city of Australia? \na. Canberra \nb. Sydney \nc. Melbourne \nAnswer: ") .lower() .strip()
+if answer3 == "a" or answer3 == "canberra": 
   score  += 1 
   print("correct!")
   print("score: ", score)
   print("\n")
 else:
-  print("incorrect ! The answer is Brisbane. ")
+  print("incorrect ! The answer is Canberra. ")
   print("score: ", score)
   print("\n")
+
+
+  # 3 questions about 
+
+  # QUESTION 4 
+answer3 = input("what is the capital city of Australia? \na. Canberra \nb. Sydney \nc. Melbourne \nAnswer: ") .lower() .strip()
+if answer3 == "a" or answer3 == "Canberra": 
+  score  += 1 
+  print("correct!")
+  print("score: ", score)
+  print("\n")
+else:
+  print("incorrect ! The answer is Canberra. ")
+  print("score: ", score)
+  print("\n")
+  
+
+
+
+
 
 
 
