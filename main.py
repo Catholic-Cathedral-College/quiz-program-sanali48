392297f3326cf0eec4dcf8ccdfc66d42babdd065
SanaliP <112032100+sanali48@users.noreply.github.com>
2022-10-03 00:13:56 +0000

    adding more questions

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# quiz.py
 name = input ("enter your name : ")
 print("hello",name,"welcome to the quiz world")
 playing = input ("do you want to play? ") .lower() .strip()
@@ -10,75 +11,121 @@ print ("okay! Let's play!")
 print("\n")
 
 def print_instructions():
-  print ("thank you for choosing to play this game")
-  print("in this game you will be tested on your general knowlege")
-  print("you could choose the answer by either using abc or typing the answer")
+  print ("Thank you for choosing to play this game 🤩")
+  print("In this game you will be tested on your general knowlege")
+  print("you will be given two main topics to choose from and you can choose them by typing either a or b")
+  print("\n")
+  print("for some questions you will be given hints, so make sure to use them if you dont know the answer 🤔")
+  print("for each main topic there will be 10 questions")
   print("\n")
 
 
 score = 0 
 print_instructions()
 
-# QUESTION 1 
-answer1 = input("what is the capital of Queensland? \na. Townsville \nb. Brisbane \nc. Noosa \nAnswer: ") .lower() .strip()
-if answer1 == "b" or answer1 == "brisbane": 
-  score  += 1 
-  print("correct!")
-  print("score: ", score)
-  print("\n")
-else:
-  print("incorrect ! The answer is Brisbane. ")
-  print("score: ", score)
-  print("\n")
 
 
-#QUESTION 2 
-answer2 = input("In which state is Briyedsville found? \na. New South Wales \nb. South Australia  \nc. Queensland \nAnswer: ") .lower() .strip()  
-if answer2 == "c" or answer2 == "queensland": 
-  score  += 1 
-  print("correct!")
-  print("score: ", score)
-  print("\n")
-else:
-  print("incorrect ! The answer is Queensland. ")
-  print("score: ", score)
-  print("\n")
+import pathlib
+import random
+from string import ascii_lowercase
+
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+NUM_QUESTIONS_PER_QUIZ = 5
+QUESTIONS_PATH = pathlib.Path(__file__).parent / "questions.toml"
 
 
-# QUESTION 3 
-answer3 = input("what is the capital city of Australia? \na. Canberra \nb. Sydney \nc. Melbourne \nAnswer: ") .lower() .strip()
-if answer3 == "a" or answer3 == "canberra": 
-  score  += 1 
-  print("correct!")
-  print("score: ", score)
-  print("\n")
-else:
-  print("incorrect ! The answer is Canberra. ")
-  print("score: ", score)
-  print("\n")
+def run_quiz():
+    questions = prepare_questions(
+        QUESTIONS_PATH, num_questions=NUM_QUESTIONS_PER_QUIZ
+    )
+
+    num_correct = 0
+    for num, question in enumerate(questions, start=1):
+        print(f"\nQuestion {num}:")
+        num_correct += ask_question(question)
+
+    print(f"\nYou got {num_correct} correct out of {num} questions")
 
 
-  # 3 questions about 
+def prepare_questions(path, num_questions):
+    topic_info = tomllib.loads(path.read_text())
+    topics = {
+        topic["label"]: topic["questions"] for topic in topic_info.values()
+    }
+    topic_label = get_answers(
+        question="Which topic do you want to be quizzed about",
+        alternatives=sorted(topics),
+    )[0]
 
-  # QUESTION 4 
-answer3 = input("what is the capital city of Australia? \na. Canberra \nb. Sydney \nc. Melbourne \nAnswer: ") .lower() .strip()
-if answer3 == "a" or answer3 == "Canberra": 
-  score  += 1 
-  print("correct!")
-  print("score: ", score)
-  print("\n")
-else:
-  print("incorrect ! The answer is Canberra. ")
-  print("score: ", score)
-  print("\n")
-  
-
+    questions = topics[topic_label]
+    num_questions = min(num_questions, len(questions))
+    return random.sample(questions, k=num_questions)
 
 
+def ask_question(question):
+    correct_answers = question["answers"]
+    alternatives = question["answers"] + question["alternatives"]
+    ordered_alternatives = random.sample(alternatives, k=len(alternatives))
+
+    answers = get_answers(
+        question=question["question"],
+        alternatives=ordered_alternatives,
+        num_choices=len(correct_answers),
+        hint=question.get("hint"),
+    )
+    if correct := (set(answers) == set(correct_answers)):
+        print("⭐ Correct! ⭐")
+    else:
+        is_or_are = " is" if len(correct_answers) == 1 else "s are"
+        print("\n- ".join([f"No, the answer{is_or_are}:"] + correct_answers))
+
+    if "explanation" in question:
+        print(f"\nEXPLANATION:\n{question['explanation']}")
+
+    return 1 if correct else 0
 
 
+def get_answers(question, alternatives, num_choices=1, hint=None):
+    print(f"{question}?")
+    labeled_alternatives = dict(zip(ascii_lowercase, alternatives))
+    if hint:
+        labeled_alternatives["?"] = "Hint"
+
+    for label, alternative in labeled_alternatives.items():
+        print(f"  {label}) {alternative}")
+
+    while True:
+        plural_s = "" if num_choices == 1 else f"s (choose {num_choices})"
+        answer = input(f"\nChoice{plural_s}? ")
+        answers = set(answer.replace(",", " ").split())
+
+        # Handle hints
+        if hint and "?" in answers:
+            print(f"\nHINT: {hint}")
+            continue
+
+        # Handle invalid answers
+        if len(answers) != num_choices:
+            plural_s = "" if num_choices == 1 else "s, separated by comma"
+            print(f"Please answer {num_choices} alternative{plural_s}")
+            continue
+
+        if any(
+            (invalid := answer) not in labeled_alternatives
+            for answer in answers
+        ):
+            print(
+                f"{[0]} is not a valid choice. "
+                f"Please use {', '.join(labeled_alternatives)}"
+            )
+            continue
+
+        return [labeled_alternatives[answer] for answer in answers]
 
 
-
-
-
+if __name__ == "__main__":
+    run_quiz() 
